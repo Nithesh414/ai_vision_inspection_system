ecceea56a728411bd6fcab04f69c9c87dea0a1fc
Nithesh <selvarajselvaraj9995@gmail.com>
2026-07-22 10:52:16 +0530

    Update model_version.py

--- backend/app/models/model_version.py
+++ backend/app/models/model_version.py
@@ -4,55 +4,5 @@ from datetime import datetime
 from app.db.session import Base
 
 
-class ModelVersion(Base):
-
-    __tablename__ = "model_versions"
 
 
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-
-
-    model_name = Column(
-        String,
-        default="Industrial_AI_Model"
-    )
-
-
-    version = Column(
-        String,
-        default="v1.0"
-    )
-
-
-    accuracy = Column(
-        Float,
-        default=0
-    )
-
-
-    loss = Column(
-        Float,
-        default=0
-    )
-
-
-    path = Column(
-        String,
-        nullable=True
-    )
-
-
-    active = Column(
-        Boolean,
-        default=False
-    )
-
-
-    created_at = Column(
-        DateTime,
-        default=datetime.utcnow
-    )
